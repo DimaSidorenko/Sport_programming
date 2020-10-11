4967be98ea59deac0671e7d2ed1c354a2af9551a
Dmitriy Sidorenko <60537382+DimaSidorenko@users.noreply.github.com>
2020-10-11 13:17:36 +0500

    Update Fenwik.cpp

--- Fenwik.cpp
+++ Fenwik.cpp
@@ -1,18 +1,20 @@
-void inc(int i1, int j1, int change = 1) {
-	for (int i = i1; i < n; i = i | (i + 1))
-		for (int j = j1; j < n; j = j | (j + 1)) {
-			t[i][j] += change;
-		}
+ll t[NMAX];
+
+void inc(int i1, int change = 1) {
+	for (int i = i1; i < NMAX; i = i | (i + 1)) {
+		t[i] += change;
+	}
 }
 
-ll prefix(int i1, int j1) {
+ll prefix(int i1) {
 	ll res = 0;
-	for (int i = i1; i >= 0; i = (i & (i + 1)) - 1)
-		for (int j = j1; j >= 0; j = (j & (j + 1)) - 1)
-				res += t[i][j];
+	for (int i = i1; i >= 0; i = (i & (i + 1)) - 1) {
+		res += t[i];
+	}
 	return res;
 }
 
-ll prefix(int i1, int j1, int i2, int j2) {
-	return prefix(i2, j2) - prefix(i1, j1);
+//возвращает prefix (i1, i2]
+ll prefix(int i1, int i2) {
+	return prefix(i2) - prefix(i1);
 }

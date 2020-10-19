95fef3054c884a02bb6537ef0da748156372390e
romesful <ilya.romesful@gmail.com>
2020-10-19 23:55:24 +0500

    Create GetAreaByPoints.cpp

---
+++ GetAreaByPoints.cpp
@@ -0,0 +1,14 @@
+double get_area_by_coordinates(vector<pair<int, int>> &coordinates) // площадь многоугольника по точкам
+{
+	double sum = 0;
+	int n = coordinates.size();
+	for (int i = 0; i < n; i++)
+	{
+		int x_cur = coordinates[i].first, y_cur = coordinates[i].second;
+		int x_next = coordinates[(i + 1) % n].first, y_next = coordinates[(i + 1) % n].second;
+
+		sum += (x_next - x_cur) * (y_cur + y_next);
+	}
+
+	return sum / 2;
+}
